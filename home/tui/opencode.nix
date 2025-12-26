19c937f7413d02da0d53d327e6a3fe12ee9ca638
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2025-12-26 12:52:49 +0000

    New: added opencode

---
+++ home/tui/opencode.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+{
+  # Ensure you have enabled opencode module
+  programs.opencode.enable = true;
+
+  # Optionally, Configure settings (like theme or model)
+  programs.opencode.settings = {
+    theme = "opencode";
+  };
+}
